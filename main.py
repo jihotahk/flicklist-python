9ac39991926fbe0b068f4765dc1c05589c73d446
Jiho Tahk <jihotahk@gmail.com>
2017-01-19 19:58:57 -0600

    Finished studio 1; added random movie function to main.py

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import webapp2
+import random
 
 class Index(webapp2.RequestHandler):
 
     def getRandomMovie(self):
 
         # TODO: make a list with at least 5 movie titles
-
+        movies = ["Princess Bride", "The Departed", "Mulan", "Napoleon Dynamite", "Fargo"]
         # TODO: randomly choose one of the movies, and return it
+        picked_movie = movies[random.randint(0, len(movies)-1)]
 
-        return "The Big Lebowski"
+        return picked_movie
 
     def get(self):
         # choose a movie by invoking our new function
         movie = self.getRandomMovie()
+        tom_movie = self.getRandomMovie()
 
         # build the response string
         content = "<h1>Movie of the Day</h1>"
@@ -20,8 +23,11 @@ class Index(webapp2.RequestHandler):
 
         # TODO: pick a different random movie, and display it under
         # the heading "<h1>Tommorrow's Movie</h1>"
+        tom_content = "<h1>Tomorrow's Movie</h1>"
+        tom_content += "<p>" + tom_movie + "</p>"
 
         self.response.write(content)
+        self.response.write(tom_content)
 
 app = webapp2.WSGIApplication([
     ('/', Index)
